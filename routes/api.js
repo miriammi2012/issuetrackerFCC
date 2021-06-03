a220a5159810a562ade0c2bb4ff4fab0d14d3209
zkindest <zkindest5@gmail.com>
2021-06-03 17:21:04 +0530

    schema changes,more routes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { ObjectId } = require("mongodb");
+const { ObjectID } = require("mongodb");
 
 module.exports = function (app, myDataBase) {
   app
@@ -9,8 +9,11 @@ module.exports = function (app, myDataBase) {
     .get(async function (req, res) {
       let project = req.params.project;
       try {
-        const data = await myDataBase.findOne({ project });
-        res.json(data.issues);
+        const data = await myDataBase
+          .find({ project })
+          .sort({ created_on: 1 })
+          .toArray();
+        res.json(data);
       } catch (error) {
         res.status(500).json(error);
       }
@@ -19,33 +22,57 @@ module.exports = function (app, myDataBase) {
     .post(function (req, res) {
       let project = req.params.project;
       const data = req.body;
-      data._id = ObjectId();
-      data.open = true;
-      myDataBase.findOneAndUpdate(
-        { project },
-        {
-          $push: {
-            issues: data,
+      data.open = 1;
+      data.created_on = new Date();
+      data.updated_on = new Date();
+      data.project = project;
+      myDataBase.insertOne(data, (err, doc) => {
+        if (err) {
+          res.status(500).json(err);
+          return;
+        }
+        res.json(doc.ops[0]);
+      });
+    })
+
+    .put(function (req, res) {
+      // let project = req.params.project;
+
+      const { _id, open } = req.body;
+      myDataBase
+        .findOneAndUpdate(
+          {
+            _id: ObjectID(_id),
           },
-        },
+          {
+            $set: {
+              open: open == "false" ? 0 : 1,
+            },
+          }
+        )
+        .then((doc) => {
+          res.json(doc.value);
+        })
+        .catch((err) => {
+          res.status(500).send("error deleting doc!");
+          return;
+        });
+    })
+
+    .delete(function (req, res) {
+      // let project = req.params.project;
+      const { _id } = req.body;
+      myDataBase.findOneAndDelete(
         {
-          upsert: true,
+          _id: ObjectID(_id),
         },
         (err, doc) => {
           if (err) {
-            res.status(500).json(err);
+            res.status(500).send("error deleting doc!");
             return;
           }
           res.json(doc.value);
         }
       );
-    })
-
-    .put(function (req, res) {
-      let project = req.params.project;
-    })
-
-    .delete(function (req, res) {
-      let project = req.params.project;
     });
 };
